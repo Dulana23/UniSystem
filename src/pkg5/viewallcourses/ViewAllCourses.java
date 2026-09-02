4ecb17ba880e1e62da4e3cae177e2111ae8ed59c
Ni0906 <nipunisalika96122@gmail.com>
2026-09-02 22:41:05 +0530

    Create ViewAllCourses class with empty course list

--- src/pkg5/viewallcourses/ViewAllCourses.java
+++ src/pkg5/viewallcourses/ViewAllCourses.java
@@ -9,15 +9,7 @@ public class ViewAllCourses {
         
          ArrayList<Course> courses = new ArrayList<>();
 
-         courses.add(new Course("PST12112", "OOP Lab", 2, 1, 2));
-        courses.add(new Course("PST12113", "Database Mgt Lab", 3, 1, 2));
-        
-       System.out.println("=======COURSE LIST======");
-       System.out.println();
-       System.out.printf("%-10s %-20s %-8s %-6s %-8s%n","Code", "Course Name", "Credits", "Year", "Semester");
-       
-      
-        }
+    }
     }
     
 
