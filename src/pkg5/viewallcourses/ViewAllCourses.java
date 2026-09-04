6400cff47165a07cfec0b00491d88c92f9dcd1d9
Ni0906 <nipunisalika96122@gmail.com>
2026-09-04 14:02:33 +0530

    Add sample courses to the list

--- src/pkg5/viewallcourses/ViewAllCourses.java
+++ src/pkg5/viewallcourses/ViewAllCourses.java
@@ -8,7 +8,8 @@ public class ViewAllCourses {
     public static void main(String[] args) {
         
          ArrayList<Course> courses = new ArrayList<>();
-
+         courses.add(new Course("PST12112", "OOP Lab", 2, 1, 2));
+        courses.add(new Course("PST12113", "Database Mgt Lab", 3, 1, 2));
     }
     }
     
