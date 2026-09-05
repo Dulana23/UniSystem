de635d82bf55bd3943d20ec5a2263177b0ae1e32
H D Piyumi Dulanjalee <hdpiyumidulanjalee@gmail.com>
2026-09-05 21:57:31 +0530

    create getter for get CourseCode

--- src/main/java/com/mycompany/add_course/Course.java
+++ src/main/java/com/mycompany/add_course/Course.java
@@ -14,13 +14,25 @@ public class Course {
     private String courseName;
     private int credits;
     private int academicYear;
-    private int semester;
+    private  int semester;
     
     
     
     public Course(String courseCode,String courseName,int credits,int academicYear,int semester)
     {
+        this.courseCode=courseCode;
+        this.courseName=courseName;
+        this.credits=credits;
+        this.academicYear=academicYear;
+        this.semester=semester;
         
     }
+    
+    public String getCourseCode()
+    {
+        return courseCode;
+    }
+    
+ 
             
 }
