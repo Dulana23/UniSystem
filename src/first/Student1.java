1e144ed7a1617d8dee6f5b12e5a010f8d51dd6dd
Tharusha Nimneth <khtnimneth@gmail.com>
2026-09-13 16:39:02 +0530

    Create variable and constructors

---
+++ src/first/Student1.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package first;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author k
+ */
+public class Student1 extends Person{
+     private String studentId;
+    private String registrationNumber;
+    private String indexNumber;
+    private String degreeProgram;
+    private int currentYear;
+    private int currentSemester;
+
+    ArrayList<Student> Students = new ArrayList<Student>();
+    
+    Scanner scanner = new Scanner(System.in);
+
+    public Student1() {
+        
+    }
+
+   
+    
+    
+    public Student1(String studentId, String registrationNumber, String indexNumber,String name,String degreeProgram, int currentYear, int currentSemester, String email) {
+        super(name, email);
+        this.studentId = studentId;
+        this.registrationNumber = registrationNumber;
+        this.indexNumber = indexNumber;
+        this.degreeProgram = degreeProgram;
+        this.currentYear = currentYear;
+        this.currentSemester = currentSemester;
+    }
+
+    @Override
+    void addStudent() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
